d1a19a70850fe6391d929071527cf5131586f023
Daihana Mora <daihanamt@gmail.com>
2026-02-28 08:46:39 -0500

    correccion code sol optima

--- Parcial 1/Ejercicio 2/Sol-Optima.py
+++ Parcial 1/Ejercicio 2/Sol-Optima.py
@@ -1,26 +1,20 @@
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
         """
-        Se tuliza  dos ciclos for. El primero toma un número 
-        y el segundo lo compara con todos los que vienen después. 
-        Si se encuentra uno igual, mando True. Si termino todo y no vi nada, False.
-
-        Complejidad (Big O):
-        - Tiempo: O(n²) porque al tener un for dentro de otro for, si la lista crece, 
-          el tiempo sube al cuadrado. Es lento para listas muy largas.
-        - Espacio: O(1) porque no creé ninguna estructura extra (como un set o mapa), 
-          solo usé la memoria de la lista que ya me dieron.
-
-        Casos:
-        - Mejor caso: Que los dos primeros números sean el duplicado (acaba rápido).
-        - Peor caso: Que no haya duplicados, porque el código tiene que hacer 
-          todas las comparaciones hasta el final.
-        
+        se utilizo el has set
+        Tiempo : O(n) se recorre la lista una vez
+        Espacio: O(n) para el peor caso se almacena todos los elementos en el set
+        Utilizamos el set para obtener que la busqueda de elementos sea de tiempo
+        constante 0(1); si al iterar se encuentra un número que  ya existe en el set,
+        se confirma el duplicado
         """
-    
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] == nums[j]:
-                    return True
+        vistos = set()
+        
+        for numero in nums:
+            
+            if numero in vistos:
+                return True
+           
+            vistos.add(numero)
+            
         return False
